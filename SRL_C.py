5f41503a50f840769998c8d3d307d3044cd5cee7
KDKftw <ooo.kadoun@gmail.com>
2022-03-04 20:32:00 +0100

    srl map, cleaning URL setup

--- SRL_C.py
+++ SRL_C.py
@@ -8,14 +8,12 @@ from selenium.webdriver.support import expected_conditions as EC
 import unittest
 totalPriceXpath = "//*[@class='price-amount']"
 odNejdrazsihoSorterXpath = "//*[contains(text(), 'od nejdražšího')]"
+zobrazitNaMapeXpath = "//*[@class='c_btn white']"
 class Test_SRL_C(unittest.TestCase):
     def setUp(self):
         setUp(self)
-
     def tearDown(self):
         tearDown(self)
-
-
     def test_SRL_sort_cheapest(self):
 
         self.driver.get(URL_SRL)
@@ -47,7 +45,6 @@ class Test_SRL_C(unittest.TestCase):
         #print(cenaZajezduAllListSorted)
 
         assert cenaZajezduAllListSorted == cenaZajezduAllList
-
     def test_SRL_sort_expensive(self):
         self.driver.get(URL_SRL)
         wait = WebDriverWait(self.driver, 1500)
@@ -84,3 +81,115 @@ class Test_SRL_C(unittest.TestCase):
         print(cenaZajezduAllListSorted)
 
         assert cenaZajezduAllListSorted == cenaZajezduAllList
+    def test_SRL_map(self):
+        driver = self.driver
+        driver.get(URL_SRL)
+        wait = WebDriverWait(driver, 12)
+        time.sleep(5)
+        self.driver.maximize_window()
+        acceptConsent(driver)
+        #time.sleep(10)
+        wait.until(EC.visibility_of(self.driver.find_element_by_xpath(zobrazitNaMapeXpath))).click()
+
+
+
+        time.sleep(4)  ##try except na kolecko, pokud ok tak click, nenajde tak pokracovat dal
+        ##kolecka jsou definovany podle velikosti - small, medium, large; vzdy se meni v class name
+
+        ##2x vyvolani na large kolecko (jsou destinace kde se musim kliknout na large vickrat
+        ##ve vsech except je pass aby to nespadlo kdyby se large nenasel, goal toho testu je se pres mapu proklikat na detail hotelu
+        try:
+            wait.until(EC.element_to_be_clickable((By.XPATH,
+                                                   "//*[@class='leaflet-marker-icon marker-cluster marker-cluster-large leaflet-zoom-animated leaflet-interactive']"))).click()
+
+        except NoSuchElementException:
+            print("nenasel se large kolecko")
+            pass
+        except TimeoutException:
+            print("nenasel se large kolecko")
+            pass
+        except ElementNotInteractableException:
+            print("nenasel se medium kolecko ElementNotInteractableException")
+            pass
+
+        try:
+            wait.until(EC.element_to_be_clickable((By.XPATH,
+                                                   "//*[@class='leaflet-marker-icon marker-cluster marker-cluster-large leaflet-zoom-animated leaflet-interactive']"))).click()
+
+        except NoSuchElementException:
+            print("nenasel se large kolecko")
+            pass
+        except TimeoutException:
+            print("nenasel se large kolecko")
+            pass
+        except ElementNotInteractableException:
+            print("nenasel se medium kolecko ElementNotInteractableException")
+            pass
+
+        ##Medium kolecko
+
+        try:
+            mediumKolecko = driver.find_elements_by_xpath(
+                "//*[@class='leaflet-marker-icon marker-cluster marker-cluster-medium leaflet-zoom-animated leaflet-interactive']")
+            wait.until(EC.element_to_be_clickable((By.XPATH,
+                                                   "//*[@class='leaflet-marker-icon marker-cluster marker-cluster-medium leaflet-zoom-animated leaflet-interactive']"))).click()
+            # wait.until(EC.element_to_be_clickable((By.XPATH,
+            # "//*[@class='leaflet-marker-icon marker-cluster marker-cluster-medium leaflet-zoom-animated leaflet-interactive']"))).execute_script("arguments[0].click();", mediumKolecko[1])
+        except NoSuchElementException:
+            print("nenasel se medium kolecko-NoSuchElementException")
+            pass
+        except TimeoutException:
+            print("nenasel se medium kolecko - TimeoutExceptio")
+            pass
+        except ElementNotInteractableException:
+            print("nenasel se medium kolecko ElementNotInteractableException")
+            pass
+
+        ##small kolecko
+
+        try:
+            koleckoCisloSmall = driver.find_element_by_xpath(
+                "//*[@class='leaflet-marker-icon marker-cluster marker-cluster-small leaflet-zoom-animated leaflet-interactive']")
+            koleckoCisloSmall.click()
+            wait.until(EC.element_to_be_clickable((By.XPATH,
+                                                   "//*[@class='leaflet-marker-icon marker-cluster marker-cluster-small leaflet-zoom-animated leaflet-interactive']"))).click()
+        except NoSuchElementException:
+            print("nenasel se small kolecko")
+            pass
+        except TimeoutException:
+            print("nenasel se small kolecko")
+            pass
+        except ElementNotInteractableException:
+            print("nenasel se small kolecko ElementNotInteractableException")
+            pass
+
+        time.sleep(3)
+
+        actualHotelPin = driver.find_element_by_xpath(
+            "//*[@class='leaflet-marker-icon leaflet-zoom-animated leaflet-interactive']")
+        driver.execute_script("arguments[0].click();", actualHotelPin)  ##at this point im at detail hotelu na mapě
+
+        try:
+            imgMissing = driver.find_element_by_xpath(
+                "//*[@class='f_image f_image--missing']")  ##when theres no photo on the detail on map theres actually class that says it is missing
+            if imgMissing.is_displayed():  ##so if I dont find this class = good
+                hotelBubble = driver.find_element_by_xpath("//*[@class='leaflet-popup-content'] //*[@class='f_bubble']")
+                msg = "V mape v bublibně hotelu se nezobrazuje fotka hotelu " + hotelBubble.text
+                sendEmail(msg)
+
+        except NoSuchElementException:
+            print("actually OK")
+
+
+        hotelBubble = driver.find_element_by_xpath("//*[@class='leaflet-popup-content'] //*[@class='f_bubble']")
+        hotelBubble.click()
+
+        time.sleep(5)
+
+
+        #assert (self.driver.current_url) != URL_SRL
+        self.driver.switch_to.window(self.driver.window_handles[1]) ##gotta switch to new window
+        currentUrl = self.driver.current_url
+        print(currentUrl)
+        print(URL_SRL)
+        assert currentUrl != URL_SRL
