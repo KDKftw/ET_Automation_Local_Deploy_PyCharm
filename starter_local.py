9dd97f68e43cc2759c2858ec4ff0b179636f7225
KDKftw <ooo.kadoun@gmail.com>
2022-04-23 22:24:01 +0200

    no message

--- starter_local.py
+++ starter_local.py
@@ -14,7 +14,6 @@ import HtmlTestRunner
 
 def suite():
     suite = unittest.TestSuite()
-
     #suite.addTest(TestDetailHotelu_D("test_detail_D"))
     suite.addTest(TestDetailHotelu_C("test_detail_fotka"))
     suite.addTest(TestDetailHotelu_C("test_detail_terminy_filtr_meal"))
@@ -24,13 +23,11 @@ def suite():
     suite.addTest(Test_Fulltext_C("test_fulltext_naseptavac"))
     suite.addTest(Test_Fulltext_C("test_fulltext_results_status_check"))
     suite.addTest(Test_Groupsearch_D("test_groupsearch_D"))
-
     suite.addTest(Test_HP_C('test_HP_zlutak_to_SRL'))
     suite.addTest(Test_HP_C('test_HP_zlutak_to_groupsearch'))
     suite.addTest(Test_HP_C('test_HP_banner_destination_to_SDO'))
     suite.addTest(Test_HP_D("test_HP_D"))
     suite.addTest(Test_LM_D("test_LM_D"))
-
     suite.addTest(TestSDO_D('test_SDO_D'))
     suite.addTest(Test_SRL_C('test_SRL_sort_cheapest'))
     suite.addTest(Test_SRL_C('test_SRL_sort_expensive'))
@@ -38,10 +35,7 @@ def suite():
     suite.addTest(Test_SRL_C('test_SRL_filtr_strava'))
     suite.addTest(Test_SRL_C('test_srl_C'))
     suite.addTest(TestSRL_D('test_SRL_D'))
-
     return suite
-
-
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
